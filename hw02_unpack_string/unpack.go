858e602182513c61d6ecd84ab81e51782d2eaff4
evgenii1452 <41251961+evgenii1452@users.noreply.github.com>
2023-12-17 11:04:44 +0500

    HW2 is completed (#2)
    * HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var result strings.Builder
+	var prevRune rune
+
+	if err := validate(str); err != nil {
+		return "", err
+	}
+
+	for i, currentRune := range str {
+		if i == 0 {
+			prevRune = currentRune
+			continue
+		}
+
+		if value, _ := strconv.Atoi(string(currentRune)); unicode.IsDigit(currentRune) && value > 0 {
+			result.WriteString(strings.Repeat(string(prevRune), value))
+		}
+
+		if !unicode.IsDigit(currentRune) && !unicode.IsDigit(prevRune) {
+			result.WriteString(string(prevRune))
+		}
+
+		prevRune = currentRune
+	}
+
+	if len(str) > 0 && !unicode.IsDigit(prevRune) {
+		result.WriteString(string(prevRune))
+	}
+
+	return result.String(), nil
+}
+
+func validate(str string) error {
+	var prevRune rune
+
+	if len(str) > 0 && unicode.IsDigit(rune(str[0])) {
+		return ErrInvalidString
+	}
+
+	for i, currentRune := range str {
+		if i != 0 && unicode.IsDigit(currentRune) && unicode.IsDigit(prevRune) {
+			return ErrInvalidString
+		}
+
+		prevRune = currentRune
+	}
+
+	return nil
 }
