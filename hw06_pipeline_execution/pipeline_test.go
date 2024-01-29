484bb983cb62bfc2aa76254474a7381d9a21bc39
Evgeny Kozlov <ekozlov@azbukavkusa.ru>
2024-01-29 11:00:52 +0500

    HW06 is done

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,37 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("close channel case", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+		maxItems := 2
+
+		go func() {
+			counter := 0
+
+			for _, v := range data {
+				in <- v
+				counter++
+
+				if counter == maxItems {
+					break
+				}
+			}
+
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, maxItems)
+		require.Equal(t, []string{"102", "104"}, result)
+		require.Less(t, int64(elapsed), int64(sleepPerStage)*int64(len(stages)+maxItems-1)+int64(fault))
+	})
 }
