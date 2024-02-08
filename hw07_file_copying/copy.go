68bc657712a2e23117dce3aa4df2c6c3338067ab
Evgeny Kozlov <ekozlov@azbukavkusa.ru>
2024-02-08 17:30:43 +0500

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,88 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnsupportedFile        = errors.New("unsupported file")
+	ErrOffsetExceedsFileSize  = errors.New("offset exceeds file size")
+	ErrOffsetCannotBeNegative = errors.New("offset cannot be negative")
+	ErrLimitCannotBeNegative  = errors.New("limit cannot be negative")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0o666)
+	if err != nil {
+		return fmt.Errorf("open file: %w", err)
+	}
+
+	defer sourceFile.Close()
+
+	fileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return fmt.Errorf("get source file info: %w", err)
+	}
+
+	err = validate(fileInfo, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 || limit+offset > fileInfo.Size() {
+		limit = fileInfo.Size() - offset
+	}
+
+	_, err = sourceFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("set offset in resource file: %w", err)
+	}
+
+	limitReader := io.LimitReader(sourceFile, limit)
+	bar := pb.Full.Start64(limit)
+	readerWithPB := bar.NewProxyReader(limitReader)
+
+	tmpFile, err := os.CreateTemp("./", "")
+	if err != nil {
+		return fmt.Errorf("create tmp file: %w", err)
+	}
+	defer tmpFile.Close()
+
+	_, err = io.Copy(tmpFile, readerWithPB)
+	if err != nil {
+		return fmt.Errorf("copy file: %w", err)
+	}
+
+	err = os.Rename(tmpFile.Name(), toPath)
+	if err != nil {
+		return fmt.Errorf("move result file: %w", err)
+	}
+
+	bar.Finish()
+
+	return nil
+}
+
+func validate(fileInfo os.FileInfo, offset, limit int64) error {
+	if offset < 0 {
+		return ErrOffsetCannotBeNegative
+	}
+
+	if limit < 0 {
+		return ErrLimitCannotBeNegative
+	}
+
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if fileInfo.IsDir() {
+		return ErrUnsupportedFile
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,114 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testFilesDir = "./testdata/"
+	testFilePath = testFilesDir + "input.txt"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	createFile := func(content []byte) *os.File {
+		file, err := os.CreateTemp("./", "")
+		if err != nil {
+			panic("Temporary file creating error")
+		}
+		defer file.Close()
+
+		_, err = file.Write(content)
+		if err != nil {
+			panic("Error writing to temporary file")
+		}
+
+		return file
+	}
+
+	copyFunction := func(content []byte, offset, limit int64) []byte {
+		fileCopy := "./copy.txt"
+		file := createFile(content)
+		defer os.Remove(file.Name())
+		defer os.Remove(fileCopy)
+
+		err := Copy(file.Name(), fileCopy, offset, limit)
+		if err != nil {
+			panic(err)
+		}
+
+		copiedContent, err := os.ReadFile(fileCopy)
+		if err != nil {
+			panic("Failed reading file copy")
+		}
+
+		return copiedContent
+	}
+
+	t.Run("success copy", func(t *testing.T) {
+		result := copyFunction([]byte("test file"), 0, 0)
+		require.Equal(t, []byte("test file"), result)
+	})
+
+	t.Run("success copy with offset", func(t *testing.T) {
+		result := copyFunction([]byte("test file"), 1, 0)
+		require.Equal(t, []byte("est file"), result)
+	})
+
+	t.Run("success copy with limit", func(t *testing.T) {
+		result := copyFunction([]byte("test file"), 0, 5)
+		require.Equal(t, []byte("test "), result)
+	})
+
+	t.Run("success copy with offset and limit", func(t *testing.T) {
+		result := copyFunction([]byte("test file"), 2, 5)
+		require.Equal(t, []byte("st fi"), result)
+	})
+
+	t.Run("success copy if limit grater file size", func(t *testing.T) {
+		result := copyFunction([]byte("test file"), 2, 100)
+		require.Equal(t, []byte("st file"), result)
+	})
+}
+
+func TestFailCopy(t *testing.T) {
+	t.Run("error if offset is negative", func(t *testing.T) {
+		err := Copy(testFilePath, "test.txt", -5, 10)
+		require.Truef(t, errors.Is(err, ErrOffsetCannotBeNegative), "actual error - %q", err)
+	})
+
+	t.Run("error if limit is negative", func(t *testing.T) {
+		err := Copy(testFilePath, "test.txt", 10, -5)
+		require.Truef(t, errors.Is(err, ErrLimitCannotBeNegative), "actual error - %q", err)
+	})
+
+	t.Run("error if input path is dir", func(t *testing.T) {
+		err := Copy(testFilesDir, "test.txt", 10, 10)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual error - %q", err)
+	})
+
+	t.Run("error if offset is grater than file size", func(t *testing.T) {
+		file, err := os.OpenFile(testFilePath, os.O_RDONLY, 0o666)
+		if err != nil {
+			panic("Test file not found!")
+		}
+		fileInfo, _ := file.Stat()
+
+		err = Copy(testFilePath, "test.txt", fileInfo.Size()+1, 10)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual error - %q", err)
+	})
+
+	t.Run("error if offset is grater than file size", func(t *testing.T) {
+		file, err := os.OpenFile(testFilePath, os.O_RDONLY, 0o666)
+		if err != nil {
+			panic("Test file not found!")
+		}
+		fileInfo, _ := file.Stat()
+
+		err = Copy(testFilePath, "test.txt", fileInfo.Size()+1, 10)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual error - %q", err)
+	})
 }
