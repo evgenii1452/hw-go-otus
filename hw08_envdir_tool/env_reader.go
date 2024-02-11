14a60dd522ef639dced1bb56482978121e5cacaf
Evgeny Kozlov <ekozlov@azbukavkusa.ru>
2024-02-11 20:41:51 +0500

    HW08 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +18,61 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func makeEnvValue(value string) EnvValue {
+	return EnvValue{
+		Value:      value,
+		NeedRemove: false,
+	}
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("read dir: %w", err)
+	}
+
+	env := make(Environment)
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		envKey := file.Name()
+
+		value, err := readFirstLine(fmt.Sprintf("%s/%s", dir, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		envVal := makeEnvValue(value)
+		if strings.TrimSpace(value) == "" {
+			envVal.NeedRemove = true
+		}
+
+		env[envKey] = envVal
+	}
+
+	return env, nil
+}
+
+func readFirstLine(filepath string) (string, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return "", fmt.Errorf("open file: %w", err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	line, _, err := reader.ReadLine()
+	if err != nil && !errors.Is(io.EOF, err) {
+		return "", fmt.Errorf("read line: %w", err)
+	}
+
+	line = bytes.ReplaceAll(line, []byte{0}, []byte{10})
+
+	return string(line), nil
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,107 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	initEnvs := func(envs map[string]string) Environment {
+		result := make(Environment)
+
+		for k, v := range envs {
+			env := EnvValue{
+				Value:      v,
+				NeedRemove: false,
+			}
+			if v == "" {
+				env.NeedRemove = true
+			}
+
+			result[k] = env
+		}
+
+		return result
+	}
+
+	createLogFile := func() *os.File {
+		logFile, err := os.Create("./logs")
+		if err != nil {
+			panic("Failed to create log file")
+		}
+
+		return logFile
+	}
+
+	getLog := func() string {
+		logFile, err := os.Open("./logs")
+		if err != nil {
+			panic("Failed to open log file")
+		}
+
+		reader := bufio.NewReader(logFile)
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			panic("Failed to read log file")
+		}
+
+		return string(line)
+	}
+
+	t.Run("test success", func(t *testing.T) {
+		basicStdout := os.Stdout
+		logFile := createLogFile()
+		defer os.Remove(logFile.Name())
+
+		envs := initEnvs(map[string]string{"TEST_ENV": "TEST"})
+
+		os.Stdout = logFile
+		result := RunCmd([]string{"/bin/bash", "./testdata/checkVarExists.sh", "TEST_ENV"}, envs)
+		os.Stdout = basicStdout
+
+		require.Equal(t, 0, result)
+		require.Equal(t, "TEST", getLog())
+	})
+
+	t.Run("test success variable not exists", func(t *testing.T) {
+		basicStdout := os.Stdout
+		logFile := createLogFile()
+		defer os.Remove(logFile.Name())
+
+		os.Stdout = logFile
+		result := RunCmd([]string{"/bin/bash", "./testdata/checkVarExists.sh", "SOME_ENV"}, make(Environment))
+		os.Stdout = basicStdout
+
+		require.Equal(t, 0, result)
+		require.Equal(t, "Variable not exists", getLog())
+	})
+
+	t.Run("test fail to few arguments", func(t *testing.T) {
+		basicStdout := os.Stdout
+		logFile := createLogFile()
+		defer os.Remove(logFile.Name())
+
+		os.Stdout = logFile
+		result := RunCmd([]string{"/bin/bash", "./testdata/checkVarExists.sh"}, make(Environment))
+		os.Stdout = basicStdout
+
+		require.Equal(t, 1, result)
+		require.Equal(t, "To few arguments", getLog())
+	})
+
+	t.Run("test fail", func(t *testing.T) {
+		basicStdout := os.Stdout
+		logFile := createLogFile()
+		defer os.Remove(logFile.Name())
+
+		os.Stdout = logFile
+		envs := initEnvs(make(map[string]string))
+		result := RunCmd([]string{"/bin/echo"}, envs)
+		os.Stdout = basicStdout
+
+		require.Equal(t, 1, result)
+	})
 }
